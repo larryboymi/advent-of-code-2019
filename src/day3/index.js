795e4f405bc17d99bd978d347a3f4b3f7cbe799d
Larry Anderson <larryboymi@hotmail.com>
2020-11-22 13:26:04 -0600

    day3 pt 2... really inefficient, would do differently next time

--- src/day3/index.js
+++ src/day3/index.js
@@ -47,8 +47,12 @@ const trackCoordinates = (begin, direction, distance) => {
   const coordinates = []
   let position = begin
   for (let distanceTraveled = 0; distanceTraveled < distance; distanceTraveled++) {
+    const prevDistance = coordinates.length ? coordinates[coordinates.length - 1].distanceTraveled : begin.distanceTraveled
     position = travel(direction)(position)
-    coordinates.push(position)
+    coordinates.push({
+      ...position,
+      distanceTraveled: prevDistance + 1
+    })
   }
   return coordinates
 }
@@ -57,11 +61,22 @@ const getWire = changes =>
   changes.reduce((acc, { direction, distance }) => {
     acc = acc.concat(trackCoordinates(acc[acc.length - 1], direction, distance))
     return acc
-  }, [{ x: 0, y: 0}])
+  }, [{ x: 0, y: 0, distanceTraveled: 0 }])
 
 const samePoint = ({ x, y}, { x: x2, y: y2}) => x === x2 && y === y2
 
-const findIntersections = (wire1, wire2) => wire1.filter(point => wire2.some(point2 => samePoint(point, point2)))
+const findIntersections = (wire1, wire2) =>
+  wire1
+  .filter(point =>
+    wire2.some(point2 =>
+      samePoint(point, point2)
+    )
+  )
+  .map(point => ({
+    ...point,
+    distanceTraveledByBoth: point.distanceTraveled + wire2.find(point2 => samePoint(point, point2)).distanceTraveled
+  }))
+
 const getMahattanDistance = ({ x, y }) => Math.abs(x) + Math.abs(y)
 
 
@@ -79,14 +94,27 @@ const getLowestManhattan = intersections =>
     return makeCurrent(curr)
   }, null)
 
+const getLowestCombinedSteps = intersections =>
+intersections.reduce((acc, curr) => {
+  const makeCurrent = point => ({
+    ...point,
+    distanceTraveledByBoth: point.distanceTraveledByBoth
+  })
+  if (acc) {
+    return acc.distanceTraveledByBoth > curr.distanceTraveledByBoth
+    ? makeCurrent(curr)
+    : acc
+  }
+  return makeCurrent(curr)
+}, null)
+
 const runDay = async () => {
   const instructions = await getCSVArray(filePath)
   const wires = instructions.map(getWire)
-  console.log(`Got wires ${wires.length}`)
   const intersections = findIntersections(wires[0].slice(1), wires[1].slice(1))
   console.log(`Intersections = ${intersections.length}`)
-  const lowestManhattan = getLowestManhattan(intersections)
-  console.log(JSON.stringify(lowestManhattan))
+  const lowestSteps = getLowestCombinedSteps(intersections)
+  console.log(JSON.stringify(lowestSteps))
 }
 
 runDay()
